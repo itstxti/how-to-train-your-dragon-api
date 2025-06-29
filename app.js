2f14ffecb0593f420c3dd8e6c991385b0dd9f16a
tati <tgarcia797@yahoo.com>
2025-06-29 23:16:32 +0200

    add data

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const dragones = require('./data/dragones.json');
+
+app.get('/dragones', (req, res) => {
+  res.json(dragones);
+});
+
+app.get('/dragones/random', (req, res) => {
+  const randomQuote = dragones[Math.floor(Math.random() * dragones.length)];
+  res.json(randomQuote);
+});
+
+app.get('/dragones/:id', (req, res) => {
+  const quote = dragones.find(q => q.id === parseInt(req.params.id));
+  if (!quote) return res.status(404).send('Quote not found');
+  res.json(quote);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`API running at http://localhost:${PORT}`);
+});
